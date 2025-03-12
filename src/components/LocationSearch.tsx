95a7096418279014c5dbfed28590fd7502656e86
Dimitris Stavroulakis <jimstav@gmail.com>
2025-03-12 22:55:31 +0200

    Typing a Callback

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import type { Place } from "../api/Place";
 
-function LocationSearch() {
+interface LocationSearchProps {
+  onPlaceClick: (place: Place) => void;
+}
+
+function LocationSearch({ onPlaceClick }: LocationSearchProps) {
+  const [places, setPlaces] = useState<Place[]>([]);
+
   return <div>Search for a location</div>;
 }
 
