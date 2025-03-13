b63a2688d155748610542272c0e9151b8759e523
Dimitris Stavroulakis <jimstav@gmail.com>
2025-03-13 20:09:09 +0200

    Why Use Interfaces

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import type { Place } from "../api/Place";
+import { search } from "../api/search";
 
 interface LocationSearchProps {
   onPlaceClick: (place: Place) => void;
@@ -12,7 +13,7 @@ function LocationSearch({ onPlaceClick }: LocationSearchProps) {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    console.log("Need to search api...", term);
+    await search(term);
   };
 
   return (
